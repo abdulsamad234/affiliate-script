4bb06c827cb7cb2fdec30eab6f6d9cd55b504cd1
Ayo Blvd <abdulsamadaliyu17@gmail.com>
2024-07-04 03:28:58 +0300

    final script version

--- affiliate-tracker.js
+++ affiliate-tracker.js
@@ -52,92 +52,100 @@
 
     if (!affiliateProgramId) return;
 
-    const affiliateProgramResponse = await fetch(
-      // TODO: change this to your API endpoint
-      "https://pushlapgrowth.com/api/affiliates/affiliate-program?programId=" +
-        affiliateProgramId,
-      {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      }
-    );
-    const affiliateProgramData = await affiliateProgramResponse.json();
-    // console.log("Affiliate Program Data:", affiliateProgramData);
-
-    if (!affiliateProgramData) return;
-    if (!affiliateProgramData.program) return;
-
-    if (affiliateParam) {
-      // TODO: check for cookie duration from affiliate program
-      setCookie(
-        cookieName,
-        affiliateParam,
-        affiliateProgramData.program.cookieDuration ?? 60
+    try {
+      const affiliateProgramResponse = await fetch(
+        // TODO: change this to your API endpoint
+        `https://www.pushlapgrowth.com/api/affiliates/affiliate-program?programId=${affiliateProgramId}`,
+        {
+          method: "GET",
+          headers: {
+            // "Access-Control-Allow-Origin": "*",
+            "Content-Type": "application/json",
+          },
+        }
       );
-      window.affiliateId = affiliateParam;
-    } else if (existingAffiliate) {
-      window.affiliateId = existingAffiliate;
-    } else {
-      window.affiliateId = null;
-    }
+      const affiliateProgramData = await affiliateProgramResponse.json();
+      // console.log("Affiliate Program Data:", affiliateProgramData);
 
-    // console.log("Affiliate ID:", window.affiliateId);
+      if (!affiliateProgramData) return;
+      if (!affiliateProgramData.program) return;
 
-    // Function to handle referrals
-    async function handleReferral() {
-      // console.log("Got into handleReferral()");
-      const programId = document
-        .querySelector("script[data-affiliate]")
-        .getAttribute("data-program-id");
-
-      // console.log("Program ID from attribute:", programId);
-
-      const referralCode = window.affiliateId;
-      if (!programId || !referralCode) return;
-
-      try {
-        const response = await fetch(
-          // TODO: change this to your API endpoint
-          "https://pushlapgrowth.com/api/affiliates/add-click",
-          {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ programId, referralCode }),
-          }
+      if (affiliateParam) {
+        // TODO: check for cookie duration from affiliate program
+        setCookie(
+          cookieName,
+          affiliateParam,
+          affiliateProgramData.program.cookieDuration ?? 60
         );
-        const data = await response.json();
-        // console.log("Data from affiliate click:", data);
-        // if (data.referral) {
-        //   setCookie(cookieName, data.referral, data.cookie_duration);
-        //   window.affiliateId = data.referral;
-        // } else {
-        //   deleteCookie(cookieName);
-        //   window.affiliateId = null;
-        // }
-        dispatchEvent(new Event("affiliate_referral_ready"));
-      } catch (error) {
-        deleteCookie(cookieName);
+        window.affiliateId = affiliateParam;
+      } else if (existingAffiliate) {
+        window.affiliateId = existingAffiliate;
+      } else {
         window.affiliateId = null;
-        console.error("Error handling referral:", error);
       }
-    }
 
-    // console.log("Existing Affiliate:", existingAffiliate);
-    // console.log("Affiliate Param:", affiliateParam);
-    // console.log("Domain:", domain);
+      // console.log("Affiliate ID:", window.affiliateId);
 
-    if (affiliateParam && existingAffiliate !== affiliateParam) {
-      handleReferral();
-    } else if (existingAffiliate) {
-      setTimeout(
-        () => dispatchEvent(new Event("affiliate_referral_ready")),
-        1200
-      );
-    } else {
-      setTimeout(
-        () => dispatchEvent(new Event("affiliate_referral_ready")),
-        1200
-      );
+      // Function to handle referrals
+      async function handleReferral() {
+        // console.log("Got into handleReferral()");
+        const programId = document
+          .querySelector("script[data-affiliate]")
+          .getAttribute("data-program-id");
+
+        // console.log("Program ID from attribute:", programId);
+
+        const referralCode = window.affiliateId;
+        if (!programId || !referralCode) return;
+
+        try {
+          const response = await fetch(
+            // TODO: change this to your API endpoint
+            "https://www.pushlapgrowth.com/api/affiliates/add-click",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({ programId, referralCode }),
+            }
+          );
+          const data = await response.json();
+          // console.log("Data from affiliate click:", data);
+          // if (data.referral) {
+          //   setCookie(cookieName, data.referral, data.cookie_duration);
+          //   window.affiliateId = data.referral;
+          // } else {
+          //   deleteCookie(cookieName);
+          //   window.affiliateId = null;
+          // }
+          dispatchEvent(new Event("affiliate_referral_ready"));
+        } catch (error) {
+          deleteCookie(cookieName);
+          window.affiliateId = null;
+          console.error("Error handling referral:", error);
+        }
+      }
+
+      // console.log("Existing Affiliate:", existingAffiliate);
+      // console.log("Affiliate Param:", affiliateParam);
+      // console.log("Domain:", domain);
+
+      if (affiliateParam && existingAffiliate !== affiliateParam) {
+        handleReferral();
+      } else if (existingAffiliate) {
+        setTimeout(
+          () => dispatchEvent(new Event("affiliate_referral_ready")),
+          1200
+        );
+      } else {
+        setTimeout(
+          () => dispatchEvent(new Event("affiliate_referral_ready")),
+          1200
+        );
+      }
+    } catch (e) {
+      console.log("Error in script: ", e);
     }
   });
 })();
